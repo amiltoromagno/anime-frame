54480464fb2cd4ccfdf70e822c5a1b1cabf3c42c
Amilto Romagno <amiltoromagno@outlook.com>
2026-05-06 20:05:36 +0900

    chore: nuxt config for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  nitro: {
+    preset: 'cloudflare_pages',
+  },
+
   css: ['~/assets/css/app.css'],
 
   vite: {
